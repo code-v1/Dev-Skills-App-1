9fb4b1d86397e1de9b47cb7837b89d1d5f0cc66b
myDeveloperJourney <daniel@mydeveloperjourney.com>
2019-06-13 17:57:53 -0500

    writes dev skill model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+SKILL_LEVELS = (
+    (1, 'Fundamental Awareness'),
+    (2, 'Novice'),
+    (3, 'Intermediate'),
+    (4, 'Advanced'),
+    (5, 'Expert'),
+)
+
+class Skill(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField(max_length=250)
+    skill_level = models.IntegerField(
+        choices=SKILL_LEVELS,
+        default=1
+        )
