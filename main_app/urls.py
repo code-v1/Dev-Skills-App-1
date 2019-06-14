38fd82965524798b182277d8ff5814e8c958208b
myDeveloperJourney <daniel@mydeveloperjourney.com>
2019-06-14 11:03:05 -0500

    completes user story login and logout

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,7 @@ from .views import *
 urlpatterns = [
     path('', home, name="home"),
     path('signup/', signup, name="signup"),
+    path('login/', login_view, name="login"),
+    path('logout/', logout_view, name="logout"),
 ]
 
